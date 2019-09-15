efcafa3106c0565090860b04c3f9384d2fca4b8a
cazhjo <cazper.sihvonen@surikat.se>
2019-09-15 12:40:05 +0200

    first commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Drawing;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileName = string.Empty;
+            args[0] = fileName;
+            try
+            {
+                Bitmap image1 = new Bitmap(fileName);
+                Console.WriteLine(image1.Height);
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine("Error");
+            }
+        }
+    }
+}
